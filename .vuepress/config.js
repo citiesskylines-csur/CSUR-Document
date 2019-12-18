87178310f12f30453c3d6d1a6c017d8b961aeff8
Golden <littlegoldenjin@gmail.com>
2019-12-18 22:11:40 +0800

    Fix zoom-custom-imgs Problem

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
           // medium-zoom options here
           // See: https://github.com/francoischalifour/medium-zoom#options
           options: {
-            margin: 16,
+            margin: 16
           }
         }
       },
