9c5ad7e2f285333d4c58784666742483c010e015
Golden <littlegoldenjin@gmail.com>
2019-12-18 22:31:29 +0800

    为什么就是没有用呢

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,15 @@ module.exports = {
     plugins: {
         '@vuepress/back-to-top':{},
         '@vuepress/nprogress':{},
-        '@vuepress/medium-zoom':{}
+        '@vuepress/medium-zoom': {
+            selector: 'img.zoom-custom-imgs',
+            // medium-zoom options here
+            // See: https://github.com/francoischalifour/medium-zoom#options
+            options: {
+            margin: 16,
+            scrollOffset: 40
+            }
+        }
     },
     // HTML多语言配置
     locales: {
