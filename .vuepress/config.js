82fd0b569be2bafc2c9114a175d007b774292b13
Golden <littlegoldenjin@gmail.com>
2020-01-05 12:59:56 +0800

    change Reop url

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
         // 以下为可选的编辑链接选项
 
         // 假如你的文档仓库和项目本身不在一个仓库：
-        docsRepo: 'Littlegolden/CSUR-Docs',
+        docsRepo: 'citiesskylines-csur/CSUR-Docs',
         // 假如文档不是放在仓库的根目录下：
         // docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
